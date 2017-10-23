4a67cdabe4137c06628ac04cbe6cf659be8344af
michal.novoty <michal.novotny@akademiasovy.sk>
2017-10-23 10:48:42 +0200

    pondelok V 2

--- opakovanie.c
+++ opakovanie.c
@@ -3,11 +3,11 @@
 #include<stdlib.h>
 #include<string.h>
 
-#define D 50
+#define D 30
 
 int main(){
 	srand(time(NULL));
-	int i=0, j=0, z=0, n=0, found=0, origin[D], copy[D];
+	int i=0, j=0, k=0, z=0, n=0, n2=0, found=0, found2=0, origin[D], copy[D], zhodne[D];
 	for(i=0;i<D;i++)
 	{
 		origin[i]=rand()%30+1;
@@ -28,7 +28,21 @@ int main(){
 			else if(z == copy[j])
 			{
 				found ++;
-				printf("\n zhoda %d", z);
+				printf("\n nasla sa zhoda zhoda %d", z);
+				found2=0;
+				for(k=0;k<n2;k++){
+					if(z == zhodne[k])
+					{
+						found2++;
+						printf(" - zhoda v poli zhodnych cisel");
+						break;
+					}
+				}
+				if(found2==0)
+					{
+						zhodne[n2] = z;
+						n2++;
+					}
 				break;
 			}
 			
@@ -39,11 +53,16 @@ int main(){
 			n++;
 		}
 	}
-	printf("\n");
+	printf("\n\n pole jedinecnych cisel: \n");
 	for(i=0;i<n;i++)
 	{
 		printf("%d ", copy[i]);
 	}
-	
+	printf("\n\n pole zhodnych cisel: \n");
+	for(i=0;i<n2;i++)
+	{
+		printf("%d ", zhodne[i]);
+	}
+	printf("\n");
 return 0;
 }
