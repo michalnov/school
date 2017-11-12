89d056694b42f5d19ccfeb0b2d5ac936a3e52d7c
michal.novotny <michal.novotny@akademiasovy.sk>
2017-11-12 13:31:15 +0100

    DONE - nobody know how and why

--- iban.c
+++ iban.c
@@ -7,16 +7,34 @@
 
 #define D 24
 
+void itoaA(int abc, char string[])
+{
+  int a = strlen(string), x = abc;
+  if (abc < 10) {
+    string[0] = x+48 ;
+    string[1] = '\0';
+  }
+  else
+  {
+    string[0] = (x/10)+48 ;
+    string[1] = (x%10)+48 ;
+    string[2] = '\0';
+  }
+}
+
 int main(int argc, char const *argv[]) {
 
+  int len=0;
   char bankCode[5], prefix[7];
-  char accountNumber[11], country[3];
-  char copy[29];
+  char accountNumber[12], country[3];
+  char copy[30];
+  copy[0] = '\0';
   int i = 0;
   strcpy(country, "SK");
   strcpy(bankCode, "0000");
   strcpy(prefix, "000000");
   strcpy(accountNumber, "0000000000");
+
   printf("\n%s", "Bank code: ");
   scanf("%s", bankCode);
   if (atoi(bankCode)==0) {
@@ -27,7 +45,7 @@ int main(int argc, char const *argv[]) {
     printf("%s\n", "Wrong Input");
     return  1;
   }
-  int len = strlen(bankCode);
+  len = strlen(bankCode);
   while (len < 4) {
     for (i = 4; i >= 0; i--) {
       if (i == 0) {
@@ -69,15 +87,15 @@ int main(int argc, char const *argv[]) {
   }
   printf("%s", "Account Number: ");
   scanf("%s", accountNumber);
-  if (strlen(accountNumber) > 9 || atoi(accountNumber) == 0) {
+  if (strlen(accountNumber) > 10 || atoi(accountNumber) == 0) {
     printf("%s\n", "Wrong Input");
     return  1;
   }
   else
   {
     len = strlen(accountNumber);
-    while (len < 9) {
-      for (i = 9; i >= 0; i--) {
+    while (len < 10) {
+      for (i = 10; i >= 0; i--) {
         if (i == 0) {
           accountNumber[0] = '0';
         }
@@ -89,16 +107,24 @@ int main(int argc, char const *argv[]) {
       }
     }
   }
+
+  //testing
+  //strcpy(prefix, "000000");
+  //strcpy(bankCode, "7500");
+  //strcpy(accountNumber, "1231231230");
+
+
   strcat(copy, bankCode);
   strcat(copy, prefix);
   strcat(copy, accountNumber);
-  printf("%s\n", copy);
+  //printf("%s\n", copy);
   char postfix[9];
   char iban[30];
   strcpy(iban, "SK00");
   strcat(iban, copy);
                           //START CALCULATING
-  char todivide[5], swap[4], decimal[12];
+  char todivide[5], swap[4], decimal[15], full[30];
+  strcpy(full, "");
   strcpy(decimal, "0.");
   strcpy(todivide, "0");
   strcpy(postfix,"282000");
@@ -109,42 +135,83 @@ int main(int argc, char const *argv[]) {
   len = strlen(copy);
                           //dividing
   do {
-    if (a < len) {
+    swap[0] = copy[a];
+    swap[1] = '\0';
+    a++;
+
+    strcat(todivide, swap);
+    //printf("%s\n", todivide);
+  } while(atoi(todivide) < 97);
+  toCalc = atoi(todivide);
+  residuum = toCalc / 97;
+  res = toCalc - ((int)residuum*97);
+  //printf("toCalc %d residuum %lf res %d\n", toCalc, residuum, res);
+  if (a < len) {
+    itoaA((int)residuum, swap);
+    strcat(full, swap);
+    itoaA(res, todivide);
+  }
+  else
+  {
+    itoaA((int)residuum, swap);
+    strcat(decimal, swap);
+    b++;
+    swap[0] = '0';
+    swap[1] = '\0';
+    itoaA(res, todivide);
+  }
+  do {
+    if (a <= len) {
       swap[0] = copy[a];
       swap[1] = '\0';
       a++;
+      //printf("%s\n", todivide);
+      strcat(todivide, swap);
+      //printf("%s\n", todivide);
     }
     else
     {
       swap[0] = '0';
       swap[1] = '\0';
+      //printf("%s\n", todivide);
+      strcat(todivide, swap);
     }
-    strcat(todivide, swap);
     toCalc = atoi(todivide);
     residuum = toCalc / 97;
     res = toCalc - ((int)residuum*97);
-    if (a < len) {
-      itoa(res, todivide, 10);
+    //printf("toCalc %d residuum %lf res %d\n", toCalc, residuum, res);
+    if (a <= len) {
+      itoaA((int)residuum, swap);
+      strcat(full, swap);
+      itoaA(res, todivide);
     }
     else
     {
-      itoa((int)residuum, swap, 10);
+      itoaA((int)residuum, swap);
+      //printf("decimal %d %s\n", b, decimal);
       strcat(decimal, swap);
       b++;
       swap[0] = '0';
       swap[1] = '\0';
+      itoaA(res, todivide);
     }
-  } while(b < 8);
+  } while(b < 11);
+  for (i = 2; i < strlen(decimal); i++) {
+    decimal[i] = decimal[i+1];
+  }
+  decimal[12] = '\0';
   result = (double)atof(decimal);
+  //printf("%s %s\n", full, decimal);
   toCalc = 97 * result;
   res = 98 - toCalc;
   if (res < 0) {
     res = res * (-1);
   }
-  itoa(res, todivide, 10);
-  printf("%s\n", todivide);
+  itoaA(res, todivide);
+  //res = res - 1;
+  //printf("%s\n", todivide);
   iban[2] = todivide[0];
-  iban[3] = todivide[1];
+  iban[3] = todivide[1]-1;
   printf("%s\n", iban);
   return 0;
 }
