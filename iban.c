bfcb016cdbfc1c528bca1d1ef4ffcbc4f5224b7d
michal.novotny <michal.novotny@akademiasovy.sk>
2017-11-12 14:31:05 +0100

    ... there is nothing

--- iban.c
+++ iban.c
@@ -207,15 +207,22 @@ int main(int argc, char const *argv[]) {
   if (res < 0) {
     res = res * (-1);
   }
-  printf("%d\n", res);
+  //printf("%d\n", res);
   itoaA(res, todivide);
   if (res == 0) {
     iban[2] = '0';
     iban[3] = '0';
+    printf("%s\n", iban);
+    return 0;
   }
   else if (res < 10) {
+    res = res - 1;
+    itoaA(res, todivide);
     iban[2] = '0';
     iban[3] = todivide[0];
+    printf("%s\n", iban);
+    return 0;
+
   }
   else
   {
